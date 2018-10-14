f174230dcbd901dda528a0708776568d25728def
Amin <El-Rifai>
2018-10-14 22:31:32 +0200

    initital commit

--- .config/dev.config.js
+++ .config/dev.config.js
@@ -1,9 +1,13 @@
 const merge = require('webpack-merge')
 const path = require('path');
+const dirTree = require('directory-tree');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const __ROOT = require('app-root-path').path;
 const __APP = path.join(__ROOT, "app");
 const __DIST = path.join(__ROOT, "dist");
+let pages = [];
 
 const config = merge(require('./common.config.js'), {
   mode: 'development',
@@ -26,7 +30,26 @@ const config = merge(require('./common.config.js'), {
         ]
       }
     ]
-  }
+  },
+
+  plugins: []
 });
+
+
+const tree = dirTree(path.join(__ROOT, "app/views/"), {extensions:/\.html$/}, (item) => {
+    pages.push(item.name.replace(/\.[^/.]+$/, ""));
+});
+
+
+pages.forEach((file) => {
+
+  config.plugins.push(
+    new HtmlWebpackPlugin({
+      template: path.join(__APP, `views/${file}.html`),
+      inject: 'true',
+      filename: path.join(__DIST, `${file}.html`),
+    })
+  );
+}),
 
 module.exports = config;
